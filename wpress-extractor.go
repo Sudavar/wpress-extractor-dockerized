7c1910ac5bd0c16c7e91af1ab3f829695f920dbb
Yury Kherimyan <yury@pressidium.com>
2025-09-13 17:48:38 +0300

    Minor first changes

--- wpress-extractor.go
+++ wpress-extractor.go
@@ -2,32 +2,27 @@ package main
 
 import (
 	"fmt"
-	"github.com/yani-/wpress"
 	"os"
+
+	"github.com/yani-/wpress"
 )
 
 func main() {
-	fmt.Printf("Wpress Extracter.\n")
 
-	if ( len(os.Args) == 2 ){
+	if ( len(os.Args) == 2 ) {
 		pathTofile := os.Args[1]
-		fmt.Println(pathTofile);
+		fmt.Printf("Attempting to extract %s", pathTofile);
+
 		archiver, _ := wpress.NewReader(pathTofile)
+
 		_ , err := archiver.Extract();
-		if (err!=nil){
+		if ( err != nil ) {
 			fmt.Println("Error = ");
 			fmt.Println(err);
-		}else{
+		} else {
 			fmt.Println("All done!");
 		}
-
-		
-		// fmt.Println("total files = ", i, " files read = ", x);
-	}else{
-		fmt.Println("Inorder to run the extractor please provide the path to the .wpress file as the first argument.");
+	} else {
+		fmt.Println("Please provide the name/path of .wpress file as the first argument");
 	}
-	
-	// wpress.Init(archiver);
-
-
 }
